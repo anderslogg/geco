d8777d556ee15415c9a1ddcefb8e7c9e954326a4
Ellery Ames <amesellery@gmail.com>
2017-05-30 19:29:04 +0200

    Put find index within loop over solution directories to account for different data-indices in different data sets (say a new data value was added at some point). Some new functions which plot the radii of support.

--- geco/gecoplotlib.py
+++ geco/gecoplotlib.py
@@ -20,13 +20,18 @@ from matplotlib import rcParams
 
 def normalized_redshift_vs_radius_ratio(sol_dirs, labels):
     "Plots the normalized redshift vs the ri/ro, as in Ansorg et al."
-    
-    e0_index       = get_data_index('E0', sol_dirs[0])
-    rinner_index   = get_data_index('r_inner', sol_dirs[0])
-    router_index   = get_data_index('r_outer', sol_dirs[0])
-    redshift_index = get_data_index('central_redshift', sol_dirs[0])    
 
     for sol_dir in sol_dirs:
+
+        try:
+            e0_index       = get_data_index('E0', sol_dir)
+            rinner_index   = get_data_index('r_inner', sol_dir)
+            router_index   = get_data_index('r_outer', sol_dir)
+            redshift_index = get_data_index('central_redshift', sol_dir)
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue            
+            
         e0_data, ri_data, ro_data, rs_data = \
           np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
           usecols=(e0_index, rinner_index,router_index, redshift_index), \
@@ -54,17 +59,24 @@ def get_data_index(data, sol_dir):
     
     header = np.loadtxt(sol_dir + '/data.csv', dtype=str, delimiter=',')
     names = header[0,:].tolist()
-    data_index = names.index(data)
-    
-    return data_index
+    try:
+        data_index = names.index(data)
+        return data_index
+    except ValueError:
+        raise ValueError
 
 def angular_momentum_vs_E0(sol_dirs):
     "Plots the angular momentum vs E0"
-    
-    e0_index  = get_data_index('E0', sol_dirs[0])
-    J_index   = get_data_index('total_angular_momentum', sol_dirs[0])
 
     for sol_dir in sol_dirs:
+
+        try:
+            e0_index  = get_data_index('E0', sol_dir)
+            J_index   = get_data_index('total_angular_momentum', sol_dir)
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue
+        
         e0_data, j_data = \
           np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
           usecols=(e0_index, J_index), \
@@ -79,11 +91,16 @@ def angular_momentum_vs_E0(sol_dirs):
 
 def frac_binding_energy_vs_E0(sol_dirs):
     "Plots the fractional binding energy vs E0"
-    
-    e0_index = get_data_index('E0', sol_dirs[0])
-    Eb_index = get_data_index('frac_binding_energy', sol_dirs[0])
 
     for sol_dir in sol_dirs:
+
+        try:
+            e0_index = get_data_index('E0', sol_dir)
+            Eb_index = get_data_index('frac_binding_energy', sol_dir)
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue
+        
         e0_data, eb_data = \
           np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
           usecols=(e0_index, Eb_index), \
@@ -98,11 +115,16 @@ def frac_binding_energy_vs_E0(sol_dirs):
 
 def normalized_central_redshift_vs_E0(sol_dirs):
     "Plots the normalized central redshift vs E0"
-    
-    e0_index = get_data_index('E0', sol_dirs[0])
-    Zc_index = get_data_index('central_redshift', sol_dirs[0])    
 
     for sol_dir in sol_dirs:
+
+        try:
+            e0_index = get_data_index('E0', sol_dir)
+            Zc_index = get_data_index('central_redshift', sol_dir)
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue             
+    
         e0_data, zc_data = \
           np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
           usecols=(e0_index, Zc_index), \
@@ -117,21 +139,35 @@ def normalized_central_redshift_vs_E0(sol_dirs):
 
     plt.show()    
 
-def normalized_central_redshift_vs_frac_binding_energy(sol_dirs):
+def normalized_central_redshift_vs_frac_binding_energy(sol_dirs, labels):
     "Plots the normalized redshift vs fractional binding energy"
-    
-    Zc_index = get_data_index('central_redshift', sol_dirs[0])    
-    Eb_index = get_data_index('frac_binding_energy', sol_dirs[0])
 
     for sol_dir in sol_dirs:
-        zc_data, eb_data = \
+
+        try:
+            e0_index = get_data_index('E0', sol_dir)            
+            Zc_index = get_data_index('central_redshift', sol_dir)    
+            Eb_index = get_data_index('frac_binding_energy', sol_dir)
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue             
+            
+        e0_data, zc_data, eb_data = \
           np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
-          usecols=(Zc_index, Eb_index), \
+          usecols=(e0_index, Zc_index, Eb_index), \
           unpack=True)
 
-        zc_norm = zc_data/(1. + zc_data)
+        e0_iter = np.array([e0_data]).reshape(-1)
+        eb_iter = np.array([eb_data]).reshape(-1)
+        zc_iter = np.array([zc_data]).reshape(-1)           
+        zc_norm = [rsd/(1. + rsd) for rsd in zc_iter]
           
-        plt.plot(eb_data, zc_norm,':ro')
+        plt.plot(eb_iter, zc_norm,':ro')
+
+        if labels:
+            for label, x, y in zip(e0_iter, eb_iter, zc_norm):
+                plt.annotate(str(label), xy=(x, y), xytext=(-2, 2),
+                            textcoords='offset points', ha='left', va='bottom')         
         
     plt.xlabel('Fractional Binding Energy $E_b$')
     plt.ylabel('Normalized Central Redshift, $Z_c/(1 + Z_c)$')
@@ -140,11 +176,16 @@ def normalized_central_redshift_vs_frac_binding_energy(sol_dirs):
 
 def normalized_central_redshift_vs_M_squared_over_J(sol_dirs):
     "Plots the normalized redshift vs M^2/J"
-    
-    Zc_index = get_data_index('central_redshift', sol_dirs[0])    
-    Jt_index = get_data_index('total_angular_momentum', sol_dirs[0])
 
     for sol_dir in sol_dirs:
+
+        try:
+            Zc_index = get_data_index('central_redshift', sol_dir)    
+            Jt_index = get_data_index('total_angular_momentum', sol_dir)
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue            
+        
         zc_data, jt_data = \
           np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
           usecols=(Zc_index, Jt_index), \
@@ -161,13 +202,18 @@ def normalized_central_redshift_vs_M_squared_over_J(sol_dirs):
     plt.show()      
 
 def M_over_Rcirc_vs_E0(sol_dirs):
-    "Plots M/Rcirc vs E0"
-    
-    e0_index      = get_data_index('E0', sol_dirs[0])
-    Rcirc_index   = get_data_index('Rcirc', sol_dirs[0])
-    m_index       = get_data_index('mass', sol_dirs[0])    
+    "Plots M/Rcirc vs E0"     
 
     for sol_dir in sol_dirs:
+
+        try:
+            e0_index      = get_data_index('E0', sol_dir)
+            Rcirc_index   = get_data_index('Rcirc', sol_dir)
+            m_index       = get_data_index('mass', sol_dir)
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue
+        
         e0_data, rc_data, m_data = \
           np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
           usecols=(e0_index, Rcirc_index, m_index), \
@@ -180,5 +226,60 @@ def M_over_Rcirc_vs_E0(sol_dirs):
     plt.ylabel('$M/R_{circ}$')
 
     plt.show()
+
+
+def r_peak_vs_E0(sol_dirs):
+    "Plots M/Rcirc vs E0"     
+
+    for sol_dir in sol_dirs:
+
+        try:
+            e0_index      = get_data_index('E0', sol_dir)
+            rpeak_index   = get_data_index('r_peak', sol_dir)
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue
+        
+        e0_data, rp_data = \
+          np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
+          usecols=(e0_index, rpeak_index), \
+          unpack=True)
+ 
+        plt.plot(e0_data, rp_data,':ro')
+        
+    plt.xlabel('$E_0$')
+    plt.ylabel('Radius peak density')
+
+    plt.show()
+
+
+def r_values_vs_E0(sol_dirs):
+    "Plots M/Rcirc vs E0"     
+
+    for sol_dir in sol_dirs:
+
+        try:
+            e0_index      = get_data_index('E0', sol_dir)
+            rp_index   = get_data_index('r_peak', sol_dir)
+            ri_index   = get_data_index('r_inner', sol_dir)
+            ro_index   = get_data_index('r_outer', sol_dir)            
+        except ValueError:
+            print "Desired data not found in '%s'" %(sol_dir)
+            continue
+        
+        e0_data, rp_data, ri_data, ro_data = \
+          np.loadtxt(sol_dir + '/data.csv', delimiter=',', skiprows=1, \
+          usecols=(e0_index, rp_index, ri_index, ro_index), \
+          unpack=True)
+ 
+        plt.plot(e0_data, rp_data,':ro')
+        plt.plot(e0_data, ri_data,':bo')
+        plt.plot(e0_data, ro_data,':go')                
+        
+    plt.xlabel('$E_0$')
+    plt.ylabel('Peak, Outer, and Inner radius of matter support')
+    plt.legend(['peak rad.', 'inner rad.', 'outer rad.'], loc='upper left')
+
+    plt.show()    
 
 
