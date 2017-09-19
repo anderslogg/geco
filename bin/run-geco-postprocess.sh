60f1f7b7f4ec05115338fcca260e6e459fbd47f8
Ellery Ames <amesellery@gmail.com>
2017-09-19 14:22:35 -0700

    Add instructions/hack for running postprocessing from a docker terminal. Include option to compute ergoregions as well.

--- bin/run-geco-postprocess.sh
+++ bin/run-geco-postprocess.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-# This script is for computing postprocess data on a sequence of GECO solutions.
+# This script is for computing postprocess data and ergoregions on a sequence of GECO solutions.
 # Usage: enter
 #   >> ./run-geco-postprocess
 # from the sequence directory (e.g. tss_L08)
+
+# From docker terminal:
+# first change geco-postprocess-data to
+# /home/fenics/shared/geco/bin/geco-postprocess-data below. Then, run
+# >> /home/fenics/shared/geco/bin/run-geco-postprocess.sh
+
 
 # For each step_ directory, compute geco-postprocess-data in that directory.
 
@@ -15,6 +21,7 @@ for STEP in $ALLSTEPS
 do
     cd $CURRENTDIR/adaptive_solver/$STEP
     geco-postprocess-data
+    # /home/fenics/shared/geco/bin/geco-postprocess-ergoregion
 done
 
 # Print a nice message
