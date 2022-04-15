d9a1142098ec787e5f7d13213d209a8130d83c13
Ellery Ames <amesellery@gmail.com>
2022-04-14 19:07:37 -0700

    Update eve_torus

--- demo/ev_torus.py
+++ demo/ev_torus.py
@@ -10,11 +10,9 @@ from geco import *
 # Create solver
 solver = EinsteinVlasovSolver()
 solver.parameters.output.plot_solution = False
-solver.parameters.discretization.radius = 50
+solver.parameters.discretization.domain_radius = 50
 solver.parameters.discretization.tolerance = 1e-4
 solver.parameters.discretization.anderson_depth = 0
-
-#solver.parameters.discretization.resolution = 64
 
 # Create ansatz for initial guess
 model = MaterialModel("EV-E-Polytropic-L-Polytropic")
@@ -31,10 +29,3 @@ model.parameters.L0 = 1.0
 
 # Compute solution
 solution = solver.solve(model, solution)
-
-# Extract solution components
-NU, BB, MU, WW, RHO, data = solution
-
-# Plot density
-plot(RHO)
-interactive()
