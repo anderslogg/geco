bf24c9ae114cf869043ab421994e5b20d7f64cf4
Ellery Ames <amesellery@gmail.com>
2022-04-25 09:20:53 -0700

    Fix parameter handling in demo vp_disk.

--- demo/vp_disk.py
+++ demo/vp_disk.py
@@ -19,30 +19,29 @@ solver.parameters["discretization"]["domain_radius"] = 50
 # Create ansatz for initial guess
 model = MaterialModel("VP-E-Polytropic-L-Polytropic")
 model.parameters["E0"] = -0.06
-#model.parameters.E0 = -0.06
-model.parameters.L0 = 0.0
-model.parameters.k = 1.0
-model.parameters.l = 1.0
+model.parameters["L0"] = 0.0
+model.parameters["k"] = 1.0
+model.parameters["l"] = 1.0
 
 # Compute solution for initial guess
-solver.parameters.output.solution_directory = out_dir + 'init_ball'
+solver.parameters["output"]["solution_directory"] = out_dir + 'init_ball'
 solution = solver.solve(model)
 
 # Create main ansatz
 model = MaterialModel("VP-E-Polytropic-L-Gaussian")
-model.parameters.E0 = -0.06
-model.parameters.pm = 1.0
-model.parameters.k = 2.4
+model.parameters["E0"] = -0.06
+model.parameters["pm"] = 1.0
+model.parameters["k"] = 2.4
 
 # Gradually decrease L0
 for L0 in list(linspace(1.4, 1.2, 3)) + list(linspace(1.2, 1.1, 3)):
     print('L0 =', L0)
 
     # Change solution output directory
-    solver.parameters.output.solution_directory = out_dir + 'L0_{}'.format(L0)
+    solver.parameters["output"]["solution_directory"] = out_dir + 'L0_{}'.format(L0)
 
     # Adjust L0
-    model.parameters.L0 = L0
+    model.parameters["L0"] = L0
 
     # Compute solution
     solution = solver.solve(model, solution)
